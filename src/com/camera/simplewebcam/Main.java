55c2f01a914085b68ceca69de8a2a903d13a567c
Samantha <sskinger@ford.com>
2012-07-24 16:48:27 -0400

    Updates to handle when the USB camera is unplugged while the app is running. Sets a dialogue box to warn user and then closes app.

--- src/com/camera/simplewebcam/Main.java
+++ src/com/camera/simplewebcam/Main.java
@@ -1,11 +1,15 @@
 package com.camera.simplewebcam;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.util.Log;
 
 
@@ -30,16 +34,28 @@ public class Main extends Activity {
         IntentFilter filter = new IntentFilter();
         filter.addAction("com.ford.openxc.HEADLAMPS_OFF");
         registerReceiver(receiver, filter);
+
+        IntentFilter usbfilter = new IntentFilter();
+        usbfilter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        registerReceiver(mUsbReceiver, usbfilter);
     }
 
     BroadcastReceiver receiver = new BroadcastReceiver() {
-
         @Override
         public void onReceive(Context context, Intent intent) {
             finish();
         }
     };
-    
+
+    BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.w("USB ERROR", "Usb device detached");
+            usbError();   
+        }
+    };
+
+
     public void finish() {
         super.finish();
     }
@@ -48,14 +64,29 @@ public class Main extends Activity {
     public void onPause() {   
         super.onPause();
         activityRunning = false;
-
+        android.os.Process.killProcess(android.os.Process.myPid()); 
     }
-    
+
     @Override
     public void onResume() {
         super.onResume();
         activityRunning = true;
-        
+    }
+
+    public void usbError(){
+        Vibrator vibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(2000);
+
+        new AlertDialog.Builder(this)
+        .setTitle("USB Camera Unplugged!")
+        .setMessage("App is closing. Please reopen from main menu.")
+        .setCancelable(false)
+        .setNeutralButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                Main.this.finish();
+            }
+        }).show();
+
     }
 
 }
