40ea92e7c4e899d455f3aefa9672b5f81023c420
Samantha <sskinger@ford.com>
2012-08-02 14:12:16 -0400

    Added a conditional in the main activity when the USB is unpluggeed to make sure activity is runnign before giving an error.

--- src/com/camera/nightvision/NightVisionActivity.java
+++ src/com/camera/nightvision/NightVisionActivity.java
@@ -21,7 +21,7 @@ public class NightVisionActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         setContentView(R.layout.main);
         activityRunning = true;
         cp = new CameraPreview(this);
@@ -76,22 +76,29 @@ public class NightVisionActivity extends Activity {
 
 
     public void usbError() {
-        Vibrator vibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
-        vibrator.vibrate(2000);
+        if(activityRunning == true) {
+            Vibrator vibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+            vibrator.vibrate(2000);
 
-        new AlertDialog.Builder(this)
-        .setTitle("USB Device Unplugged!")
-        .setMessage("Ford Night Vision is closing. Please insert a USB web camera.")
-        .setCancelable(false)
-        .setNeutralButton("OK", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                activityRunning = false;
-                android.os.Process.killProcess(android.os.Process.myPid()); 
-            }
-        }).show();
+            new AlertDialog.Builder(this)
+            .setTitle("USB Device Unplugged!")
+            .setMessage("Ford Night Vision is closing. Please insert a USB web camera.")
+            .setCancelable(false)
+            .setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    activityRunning = false;
+                    android.os.Process.killProcess(android.os.Process.myPid()); 
+                }
+            }).show();
+        }
+        else if (activityRunning == false ) {
+            Vibrator vibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+            vibrator.vibrate(2000);
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
     }
 
-    
+
     @Override
     public void onDestroy() {
         super.onDestroy();
@@ -101,19 +108,19 @@ public class NightVisionActivity extends Activity {
         unregisterReceiver(closeReceiver);
     }
 
-    
+
     public static boolean isRunning() {
         return activityRunning;
     }
 
-    
+
     public void registerCloseReceiver() {
         IntentFilter filter = new IntentFilter();
         filter.addAction("com.ford.openxc.HEADLAMPS_OFF");
         registerReceiver(closeReceiver, filter);
     }
 
-    
+
     public void registerUsbReceiver() {
         IntentFilter usbfilter = new IntentFilter();
         usbfilter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
